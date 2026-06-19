dafc9378b57b61187c02669a08e5ca76180ca6c4
Devansh Kalia <33525737+kaliadevansh@users.noreply.github.com>
2026-06-18 23:18:10 -0700

    Add: house-robber - submission-5

---
+++ Data Structures & Algorithms/house-robber/submission-5.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int rob(int[] nums) {
+        /* let us store max array.
+         * where max[i] means maximum recovery from ith house.
+        */
+        int maxSum = 0;
+        int memory[] = new int[nums.length];
+        for(int i=0; i<memory.length; i++) {
+            memory[i]=-1;
+        }
+        return getMemory(nums, memory, 0);
+    }
+    int getMemory(int[] nums, int[] memory, int s) {
+        if (s>=nums.length) {
+            return 0;
+        }
+
+        if(s==nums.length-1) {
+            return nums[s];
+        }
+        if (memory[s] == -1) {
+            memory[s] = Math.max(nums[s] + getMemory(nums, memory, s+2), getMemory(nums, memory, s+1));
+        }
+        return memory[s];
+    }
+}
